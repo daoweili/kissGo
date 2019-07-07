2a0872fdbe3d470765f3046e48dabf52307e063e
lidaowei <lidaowei@vip.qq.com>
2019-07-07 22:09:27 +0800

    init

--- excelUtils.go
+++ excelUtils.go
@@ -1,4 +1,4 @@
-package utils
+package kissGo
 
 import "github.com/360EntSecGroup-Skylar/excelize/v2"
 
@@ -6,27 +6,21 @@ func SetCell(val interface{}, index, sheet string, file *excelize.File) {
 	switch val.(type) {
 	case int:
 		if v, ok := val.(int); ok {
-			err := file.SetCellInt(sheet, index, v)
-			PrintErr(err)
+			_ = file.SetCellInt(sheet, index, v)
 		}
 	case string:
 		if v, ok := val.(string); ok {
-			err := file.SetCellStr(sheet, index, v)
-			PrintErr(err)
+			_ = file.SetCellStr(sheet, index, v)
 		}
 	case float64:
 		if v, ok := val.(float64); ok {
-			err := file.SetCellFloat(sheet, index, v, 2, 64)
-			PrintErr(err)
+			_ = file.SetCellFloat(sheet, index, v, 2, 64)
 		}
 	default:
-		err := file.SetCellDefault(sheet, index, val.(string))
-		PrintErr(err)
+		_ = file.SetCellDefault(sheet, index, val.(string))
 	}
 }
 
 func SetFloatCell(val float64, index, sheet string, prec, bitSize int, file *excelize.File) {
-	err := file.SetCellFloat(sheet, index, val, prec, bitSize)
-	PrintErr(err)
-
+	_ = file.SetCellFloat(sheet, index, val, prec, bitSize)
 }
